54557562ae60c9c27f7a4a0216482d6c545dca44
jpbp <jppenna04@gmail.com>
2025-07-09 22:25:26 -0300

    test: Add tests for invalid access token scenarios

--- tests/test_app.py
+++ tests/test_app.py
@@ -224,3 +224,21 @@ def test_login_for_access_token_valid(client, users):
     assert response.status_code == HTTPStatus.OK
     assert 'access_token' in token
     assert token['token_type'] == 'Bearer'
+
+
+def test_login_for_access_token_invalid_email(client, users):
+    response = client.post(
+        '/token',
+        data={'username': 'fake@email.com', 'password': 'senha123'},
+    )
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Incorrect email'}
+
+
+def test_login_for_access_token_invalid_password(client, users):
+    response = client.post(
+        '/token',
+        data={'username': users[0].email, 'password': 'senha123'},
+    )
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Incorrect password'}
--- tests/test_security.py
+++ tests/test_security.py
@@ -1,8 +1,14 @@
+from datetime import datetime, timedelta
 from http import HTTPStatus
 
-from jwt import decode
+from jwt import decode, encode
 
-from fastapi_zero.security import ALGORITHM, SECRET_KEY, create_access_token
+from fastapi_zero.security import (
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    ALGORITHM,
+    SECRET_KEY,
+    create_access_token,
+)
 
 
 def test_create_access_token():
@@ -17,5 +23,38 @@ def test_jwt_invalid_token(client):
     response = client.delete(
         '/users/3', headers={'Authorization': 'Bearer token-Invalido'}
     )
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_get_current_user_token_without_sub(client):
+    token_data = {
+        'user_id': 123,
+        'exp': datetime.utcnow()
+        + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
+    }
+    token = encode(token_data, SECRET_KEY, algorithm=ALGORITHM)
+
+    response = client.get(
+        '/users/', headers={'Authorization': f'Bearer {token}'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_get_current_user_token_invalid_email(client):
+    token_data = {
+        'sub': 'jp-fake@example.com',
+        'user_id': 123,
+        'exp': datetime.utcnow()
+        + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
+    }
+    token = encode(token_data, SECRET_KEY, algorithm=ALGORITHM)
+
+    response = client.get(
+        '/users/', headers={'Authorization': f'Bearer {token}'}
+    )
+
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json() == {'detail': 'Could not validate credentials'}
